a3c20850666757a4128b1cb4aa7c53278ac4587b
Samhith Kakarla <samhith.kakarla@gmail.com>
2022-03-10 16:24:09 -0500

    added state and navigation

---
+++ frontend/main.js
@@ -0,0 +1,47 @@
+import './styles.css';
+
+const App = function _App() {
+  return `
+    <nav class="bg-gray-two flex w-full justify-between items-center py-3 px-20" role="navigation">
+      <div class="flex flex-row items-center">
+        <p class="font-semibold text-md ml-3 text-white">
+          RigShowAR
+        </p>
+      </div>
+      <div class="flex flex-row">
+        <i 
+          id="dashboard-icon"
+          class="fa-solid fa-house-chimney text-xl px-6" 
+          style="color: ${_App.state.activeTab === 0 ? "#FC7636" : "#FFFFFF"}; cursor:pointer;"
+        ></i>
+        <i 
+          id="camera-icon"
+          class="fa-solid fa-camera text-xl px-6"
+          style="color: ${_App.state.activeTab === 1 ? "#FC7636" : "#FFFFFF"}; cursor:pointer;"
+        ></i>
+      </div>
+    </nav>
+  `
+};
+
+App.state = {
+  activeTab: 0,
+  changeToDashboard: () => {
+    App.state.activeTab = 0;
+    navigate();
+  },
+  changeToCamera: () => {
+    App.state.activeTab = 1;
+    navigate();
+  },
+}
+
+const navigate = () => {
+  document.getElementById("app").innerHTML = App();
+  const dashboardIcon = document.getElementById("dashboard-icon");
+  const cameraIcon = document.getElementById("camera-icon");
+  dashboardIcon.addEventListener("click", App.state.changeToDashboard);
+  cameraIcon.addEventListener("click", App.state.changeToCamera);
+};
+
+navigate();
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'gray-one': '#1F1F1F',
+        'gray-two': '#333333',
+        'gray-three': '#7B8794',
+        'orange-one': '#FDBB9B',
+        'orange-two': '#FD9868',
+        'orange-three': '#FC7636',
+      },
+    },
+  },
+  plugins: [],
+}
